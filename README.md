887dfb87b6676333680dfc0d20262534f11ef15d
miguem89 <miguelangelmutti@hotmail.com>
2024-08-20 15:29:25 -0300

    cfg y constants

---
+++ README.md
@@ -0,0 +1,6 @@
+# Challenge Data Analytics con Python
+
+
+## Prerequisitos
+### Pipenv
+Utilice pipenv para la gestion del entorno virtual y la creacion del archivo requirements.txt
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import requests
 import os 
 import datetime
 import db
-import pandas as pd 
+import pandas as pd
+from cfg import categorias
 
 
 def get_fechas_string(tipo):
@@ -37,7 +38,7 @@ def crear_carpetas(categorias):
     for categoria in categorias:
         #if not os.path.exists(path):
         periodo = get_fechas_string(1)
-        new_path = f"{categoria}/{periodo}"
+        new_path = f"{categoria['name']}/{periodo}"
         if not os.path.exists(new_path):
             os.makedirs(new_path)
     
@@ -92,18 +93,20 @@ def get_last_files_path():
 
 if __name__ == '__main__':
             
+            """
             categorias = {
 
                 'bibliotecas':'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/01c6c048-dbeb-44e0-8efa-6944f73715d7/download/11_bibliotecapopular-datos-abiertos.csv',
                 'museos': 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/4207def0-2ff7-41d5-9095-d42ae8207a5d/download/museos_datosabiertos.csv',
                 'cines': 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/f7a8edb8-9208-41b0-8f19-d72811dcea97/download/salas_cine.csv'
             }
+            """
 
             crear_carpetas(categorias)
             
-            for categoria, url in categorias.items():
-                respuesta = descargar_archivo(url)
-                guardar_archivo(respuesta, categoria)
+            for categoria in categorias:                                
+                respuesta = descargar_archivo(categoria["url"])
+                guardar_archivo(respuesta, categoria["name"])
             
             
 
